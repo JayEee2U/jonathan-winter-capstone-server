bbb099eeebfe9cc806ab4b147173877b4d33dafe
JayEee2U <j_double_u@icloud.com>
2024-01-29 22:11:53 -0500

    add knex and .env

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
+const port = 8080;
+
+
+app.use(express.json());
+app.use(cors());
+
+const events = [
+    {
+        title: 'massage appointment',
+        start: new Date(2024, 4, 7, 15),
+        end: new Date (2024, 4, 7, 16),
+        allDay: false,
+    },
+    {
+        title: 'chiropractor appointment',
+        start: new Date(2024, 4, 15, 9),
+        end: new Date (2024, 4, 15, 10, 30),
+        allDay: false,
+    },
+    {
+        title: 'massage appointment',
+        start: new Date(2024, 4, 20),
+        end: new Date (2024, 4, 20),
+        allDay: true,
+    }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/events', (req, res) => {
+    res.json(events)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
