7b6660664f19b69c37c232b634343286a52cad63
JayEee2U <j_double_u@icloud.com>
2024-02-04 16:00:17 -0500

    corected hashedpassword

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,8 @@
 const knex = require("knex")(require('../knexfile'));
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const authorize = require("../middleware/authorize");
 
 //  GET all users -----------------
 
@@ -57,7 +60,9 @@ const add = async (req, res) => {
         !req.body.first_name ||
         !req.body.last_name ||
         !req.body.date_of_birth ||
-        !req.body.email
+        !req.body.email||
+        !req.body.password
+
     ) {
       return res.status(400).json({
         message: "Please fill out all the form fields",
@@ -148,6 +153,95 @@ const medical = async(req, res) => {
     }
   };
 
+  // POST to '/users/register', creating a new user --------;
+
+  const register = async (req, res) => {
+    const { first_name, last_name, date_of_birth, email, password } = req.body;
+
+    if (!first_name || !last_name || !date_of_birth || !email || !password) {
+        return res.status(400).send('Please enter all required fields');
+    }
+  
+    const hashedPassword = bcrypt.hashSync(password, 3);
+  
+    const newUser = {
+        first_name,
+        last_name,
+        date_of_birth,
+        email,
+        password: hashedPassword,
+    };
+  
+    try {
+  
+        await knex('users').insert(newUser);
+        res.status(201).send('Registered successfully!');
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Failed registration');
+    }
+  
+};
+
+//  POST '/users/login  -----------
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+      return res.status(400).send('Plese enter all required fields');
+  }
+
+  const user = await knex('users').where({email: email }).first();
+
+  if (!user) {
+      return res.status(400).send('Invalid email');
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+  if (!isPasswordCorrect) {
+      return res.status(400).send('Invalid password');
+  }
+
+  const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY,
+      { expiresIn: '24h' }
+  );
+
+  res.json({ token: token });
+
+  res.status(200);
+};
+
+//  GET '/users/current
+
+const current = async ( req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send('Please login');
+}
+console.log(req.headers.authorization);
+
+const authHeader = req.headers.authorization;
+
+const authToken = authHeader.split(' ')[1];
+
+try {
+
+    const decoded = jwt.verify(authToken, process.env.JWT_KEY);
+    console.log(decoded); 
+
+    const user = await knex('users').where({ id: decoded.id }).first();
+
+    delete user.password;
+
+   res.json(user); 
+} catch (err) {
+    return res.status(401).send('Invalid auth token:' ,err);
+}
+}
+
 module.exports = {
     index,
     findOne,
@@ -155,5 +249,8 @@ module.exports = {
     add,
     update,
     remove,
-    medical
+    medical,
+    register,
+    login,
+    current
   }
